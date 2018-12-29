e8e7b64e1f12975a248d5eb3df1a1fb1136b8b11
chenjin0827 <1219650952@qq.com>
2018-12-29 10:33:28 +0800

    my----

--- chenjin-log4j/src/test/java/com/test/TestLog4j2.java
+++ chenjin-log4j/src/test/java/com/test/TestLog4j2.java
@@ -2,11 +2,11 @@ package com.test;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-//trace<debug<info<warn<error<fatal 顺序
+//off > fatal > error > warn > info > debug> trace > all
 public class TestLog4j2 {
     public static void main(String[] args) {
         Logger logger = LogManager.getLogger(TestLog4j2.class);
-        for (int i=0;i<10000;i++){
+        for (int i=0;i<100000;i++){
             logger.trace("trace level");
             logger.debug("debug level");
             logger.info("info level");
