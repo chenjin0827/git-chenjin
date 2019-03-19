3df67c2f9dd8f5a8ea13b414d5d3dedb056c3afb
chenjin0827 <1219650952@qq.com>
2019-03-19 13:57:01 +0800

    20190319提交

--- chenjin-springboot/src/main/java/com/springboot/controller/HelloWorldController.java
+++ chenjin-springboot/src/main/java/com/springboot/controller/HelloWorldController.java
@@ -1,13 +1,34 @@
 package com.springboot.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-    @RestController
-//    @RequestMapping("/")
-    public class HelloWorldController {
-        @RequestMapping("/")
-        public String sayHello() {
-            return "Hello,World!";
-        }
+@Controller
+@RequestMapping("/hello")
+public class HelloWorldController {
+    //模板不行 找不到模板
+    @RequestMapping("/")
+    public String sayHello() {
+        return "Hello,World!";
     }
+
+    @RequestMapping("/testHtml")
+    public String testHtml(ModelMap map) {
+        map.addAttribute("host", "http://www.baidu.com");
+        return "html";
+    }
+    @RequestMapping("/testFreeMarker")
+    public String testFreeMarker(ModelMap map) {
+        map.addAttribute("host", "http://www.baidu.com");
+        return "freeMarker";
+    }
+    @RequestMapping("/testVelocity")
+    public String testVelocity(ModelMap map) {
+        map.addAttribute("host", "http://www.baidu.com");
+        return "velocity";
+    }
+
+
+}
