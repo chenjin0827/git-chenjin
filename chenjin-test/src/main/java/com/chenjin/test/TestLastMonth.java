abf4636951e40cd651e836122c8e2d4bb1b3f02b
chenjin0827 <1219650952@qq.com>
2019-06-24 16:15:30 +0800

    20190530commit

--- chenjin-test/src/main/java/com/chenjin/test/TestLastMonth.java
+++ chenjin-test/src/main/java/com/chenjin/test/TestLastMonth.java
@@ -6,8 +6,8 @@ import java.util.Date;
 
 public class TestLastMonth {
     public static void main(String[] args) throws Exception{
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM");
-        Date date = simpleDateFormat.parse("2015-01-01");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = simpleDateFormat.parse("2019-03-31");
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         calendar.add(Calendar.MONTH,-1);
