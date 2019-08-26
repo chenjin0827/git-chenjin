ab38d16d43a8d57c3d25211184d355550da89ff0
chenjin0827 <1219650952@qq.com>
2019-08-26 14:39:01 +0800

    20190826

---
+++ chenjin-hibernate4/src/main/java/com/chenjin/hibernate4/entity/User.java
@@ -0,0 +1,50 @@
+package com.chenjin.hibernate4.entity;
+
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+    private static final long serialVersionUID = -1L;
+    @Id
+    @GenericGenerator(name = "generator", strategy = "increment")
+    @GeneratedValue(generator = "generator")
+    @Column(name = "id", nullable = false, unique = true)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    private String name;
+    private int age;
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User() {
+    }
+}
