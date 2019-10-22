6f5eed2e91f60eeb566c45911caff96259bfde4b
1219650953@qq.com <cj13032716642>
2019-10-22 11:44:17 +0800

    20191022---2

--- chenjin-test/src/test/java/com/chenjin/test/TestStringBuffer.java
+++ chenjin-test/src/test/java/com/chenjin/test/TestStringBuffer.java
@@ -1,13 +1,1 @@
-package com.chenjin.test;
-
-public class TestStringBuffer {
-    public static void main(String[] args) {
-
-        StringBuffer stringBuffer = new StringBuffer("1234");
-        System.out.println(stringBuffer);
-        stringBuffer.setLength(0);
-        System.out.println("清空后"+stringBuffer);
-        stringBuffer.append("33424365");
-        System.out.println("清空后后面"+stringBuffer);
-    }
-}
+package com.chenjin.test;public class TestStringBuffer {    public static void main(String[] args) {        StringBuffer stringBuffer = new StringBuffer("1234");        System.out.println(stringBuffer);        stringBuffer.setLength(0);        System.out.println("清空后"+stringBuffer);        stringBuffer.append("33424365");        System.out.println("清空后后面"+stringBuffer);  //aa    }}
