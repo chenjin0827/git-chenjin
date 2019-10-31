7c6eb12afbe13b0df64f5aee16d3bb415f2a0a85
1219650953@qq.com <cj13032716642>
2019-10-31 17:19:29 +0800

    20191031---

---
+++ chenjin-pattern/src/main/java/com/chenjin/pattern/adapter/DefaultAdapter.java
@@ -0,0 +1,31 @@
+package com.chenjin.pattern.adapter;
+
+/**
+ * 适配器角色
+ */
+public class DefaultAdapter implements  SampleOperation{
+    @Override
+    public void operation1() {
+
+    }
+
+    @Override
+    public void operation2() {
+
+    }
+
+    @Override
+    public void operation3() {
+
+    }
+
+    @Override
+    public void operation4() {
+
+    }
+
+    @Override
+    public void operation5() {
+
+    }
+}
