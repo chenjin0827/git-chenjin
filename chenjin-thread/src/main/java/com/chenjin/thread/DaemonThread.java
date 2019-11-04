e6b101e1e2c4a6f79930c137d1bfda77d40d2a11
1219650953@qq.com <cj13032716642>
2019-11-04 14:01:31 +0800

    20191104---

--- chenjin-thread/src/main/java/com/chenjin/thread/DaemonThread.java
+++ chenjin-thread/src/main/java/com/chenjin/thread/DaemonThread.java
@@ -2,6 +2,8 @@ package com.chenjin.thread;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * 测试守护线程   主线程休眠完了守护线程也跟着终止了
  * 守护线程的意思：和主线程共死的，主线程退出来守护线程就一定会退出
@@ -11,26 +13,28 @@ public class DaemonThread extends Thread {
 
     @Override
     public void run() {
-        try{
-            while (!isInterrupted()) {
-                System.out.println("当前线程名字是===" + Thread.currentThread().getName()+
-                        "当前线程id是==="+Thread.currentThread().getId());
-            }
-            System.out.println("线程被终止了");
-        }finally {
-            System.out.println("进入到finally===");
+        System.out.println("enter run()");
+        try {
+            System.out.println("enter try block");
+            //这里如果阻塞时间比主线程长最后就不会进入到finally 因为主线程结束了，守护线程就不管阻塞，跟着结束了
+            TimeUnit.SECONDS.sleep(5);
+            int a=1/0;
+            System.out.println(a);
+            System.out.println("执行结束==");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("enter finally block");
         }
-
     }
 
+
     @Test
-    public void testDaemonThread() throws InterruptedException {
+    public void testDaemonThread() throws Exception {
         DaemonThread daemonThread = new DaemonThread();
         //设置守护线程一定要放在start之前才能生效
-        daemonThread.setDaemon(true);
+            daemonThread.setDaemon(true);
         daemonThread.start();
-        Thread.sleep(5);
-//        daemonThread.interrupt();//不中断测不出来，相当于while一直是true，
-        // 没执行玩，就不会打印finally
+        TimeUnit.SECONDS.sleep(10);
     }
 }
