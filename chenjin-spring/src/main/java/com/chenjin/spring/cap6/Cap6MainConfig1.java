eeb8547bb631d687fc310223a3a1fd8ba5548e5a
1219650953@qq.com <cj13032716642>
2019-11-07 17:24:10 +0800

    20191107----

--- chenjin-spring/src/main/java/com/chenjin/spring/cap6/Cap6MainConfig1.java
+++ chenjin-spring/src/main/java/com/chenjin/spring/cap6/Cap6MainConfig1.java
@@ -10,15 +10,18 @@ import org.springframework.context.annotation.Import;
 
 
 @Configuration
-@Import(value = {Dog.class,MyImportSelector.class})
+@Import(value = {Dog.class, MyImportSelector.class,
+        MyImportBeanDefinitionRegistrar.class})
 public class Cap6MainConfig1 {
     /**
      * 给容器注册组件的三种方式
      * 1、@Bean
      * 2、包扫描+组件标注的注解
      * 3、@Import （快速给容器导入一个组件），@Bean有点简单
-     *  a，@Import（要导入到容器中的组件）：容器会自动注册这个组件，bean的id为全类名
-     *  b，ImportSelector：是一个接口，返回需要导入到容器的组件的全类名数组
+     * a，@Import（要导入到容器中的组件）：容器会自动注册这个组件，bean的id为全类名
+     * b，ImportSelector：是一个接口，返回需要导入到容器的组件的全类名数组
+       c,ImportBeanDefinitionRegistrar :可以手动添加组件到IOC容器，所有
+        bean的注册都可以通过它来实现
      */
     @Bean(name = "person")
     public Person person() {
--- chenjin-spring/src/main/java/com/chenjin/spring/cap6/MyImportBeanDefinitionRegistrar.java
+++ chenjin-spring/src/main/java/com/chenjin/spring/cap6/MyImportBeanDefinitionRegistrar.java
@@ -1,20 +1,26 @@
 package com.chenjin.spring.cap6;
 
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.type.AnnotationMetadata;
 
-public class MyImportBeanDefinitionRegistrar implements ImportBeanDefinitionRegistrar{
+public class MyImportBeanDefinitionRegistrar implements ImportBeanDefinitionRegistrar {
     /**
-     *自己手动注册bean
-     * @param annotationMetadata
-     * 当前类的注解信息
-     * @param registry BeanDefinition注册类，把所有需要添加到容器中的bean加入
+     * 自己手动注册bean
+     *
+     * @param annotationMetadata 当前类的注解信息
+     * @param registry           BeanDefinition注册类，把所有需要添加到容器中的bean加入
      */
     public void registerBeanDefinitions(AnnotationMetadata annotationMetadata, BeanDefinitionRegistry registry) {
-
-        boolean b = registry.containsBeanDefinition("com.chenjin.spring.cap6.bean.Pig");
-
+        boolean b1 = registry.containsBeanDefinition("com.chenjin.spring.cap6.Dog");
+        boolean b2 = registry.containsBeanDefinition("com.chenjin.spring.cap6.Cat");
+//        boolean b2 = registry.containsBeanDefinition("com.chenjin.spring.cap6.Tiger");
+        //如果Pig和tiger同时存在于IOC容器中，那么创建Monkey类，加入到容器
+        if (b1 && b2) {
+            RootBeanDefinition beanDefinition = new RootBeanDefinition(Monkey.class);
+            registry.registerBeanDefinition("monKey", beanDefinition);
+        }
 
     }
 }
