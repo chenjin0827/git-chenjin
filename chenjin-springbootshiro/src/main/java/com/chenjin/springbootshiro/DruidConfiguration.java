9a6deca21c57f4bee5f3a59d1bf5ee5420a9aa63
1219650953@qq.com <cj13032716642>
2019-11-21 10:13:35 +0800

    20191118----

--- chenjin-springbootshiro/src/main/java/com/chenjin/springbootshiro/DruidConfiguration.java
+++ chenjin-springbootshiro/src/main/java/com/chenjin/springbootshiro/DruidConfiguration.java
@@ -26,7 +26,7 @@ public class DruidConfiguration {
         //白名单(允许访问的IP)
         servletRegistrationBean.addInitParameter("allow", "127.0.0.1");
         //IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的即提示:Sorry, you are not permitted to view this page.
-        servletRegistrationBean.addInitParameter("deny", "192.168.1.100");
+        servletRegistrationBean.addInitParameter("deny", "192.168.7.19");
         //登录查看信息的账号密码.
         servletRegistrationBean.addInitParameter("loginUsername", "druid");
         servletRegistrationBean.addInitParameter("loginPassword", "12345678");
