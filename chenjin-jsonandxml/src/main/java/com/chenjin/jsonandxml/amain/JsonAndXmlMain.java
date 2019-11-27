3d1877c3a79e14a278149e1c5d7be3bf74f38fbb
1219650953@qq.com <cj13032716642>
2019-11-27 16:40:19 +0800

    20191127----

--- chenjin-jsonandxml/src/main/java/com/chenjin/jsonandxml/amain/JsonAndXmlMain.java
+++ chenjin-jsonandxml/src/main/java/com/chenjin/jsonandxml/amain/JsonAndXmlMain.java
@@ -4,7 +4,9 @@ package com.chenjin.jsonandxml.amain;
  * json和xml的相关知识
  * 1、json和jsonp的区别：
  * json是数据格式，jsonp是可以跨域的，公司网关接口
- *
+ * 2、gson是谷歌的，安卓用的比较多
+ * jackson springmvc默认返回jackson的
+ * fastjson是阿里的，目前web端用的比较多
  */
 public class JsonAndXmlMain {
 }
---
+++ chenjin-jsonandxml/src/main/java/com/chenjin/jsonandxml/json/JsonTest.java
@@ -0,0 +1,8 @@
+package com.chenjin.jsonandxml.json;
+
+public class JsonTest {
+    static String jsonStr = "{\"sites\": [{\"name\": \"蚂蚁课堂\",\"url\": \"www.itmayiedu.com\"},{\"name\": \"每特教育\",\"url\": \"http://meiteedu.com/\"}]}";
+    public static void main(String[] args) {
+
+    }
+}
