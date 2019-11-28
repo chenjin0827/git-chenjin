5c841fd60e46426b8793c66ae4c11514427d12a8
1219650953@qq.com <cj13032716642>
2019-11-28 15:38:58 +0800

    20191128----

--- chenjin-socket/src/main/java/com/chenjin/socket/TcpServer.java
+++ chenjin-socket/src/main/java/com/chenjin/socket/TcpServer.java
@@ -5,6 +5,21 @@ import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * 三次握手：
+ * 第一次握手：
+ * 服务器：客户端可以通讯了吗？
+ * 第二次握手：
+ * 客户端响应，带上信息告诉服务器可以通讯了
+ * 第三次握手：
+ * 服务器告诉客户端：那我现在开始发送数据你了
+ *
+ * 四次分手：
+ * 1、客户端发送一个标识fin到服务器，用来关闭数据传送
+ * 2、服务器收到这个fin，发回一个ack，
+ * 3、服务器关闭与客户端的连接
+ * 4、客户端发回报文确认
+ */
 public class TcpServer {
     public static void main(String[] args) throws IOException {
         System.out.println("socket服务器端启动....");
