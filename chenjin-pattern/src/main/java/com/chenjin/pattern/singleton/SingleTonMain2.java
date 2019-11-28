60ee4d20bacdac57c92e1f283c5ac06f5ef7b572
1219650953@qq.com <cj13032716642>
2019-11-28 13:48:49 +0800

    20191128----

--- chenjin-pattern/src/main/java/com/chenjin/pattern/singleton/SingleTonMain2.java
+++ chenjin-pattern/src/main/java/com/chenjin/pattern/singleton/SingleTonMain2.java
@@ -1,7 +1,8 @@
 package com.chenjin.pattern.singleton;
 
 /**
- *饿汉单例
+ * 饿汉单例(懒汉)，存在线程安全问题,
+ * 可以使用synchronized同步锁实现多线程下也线程安全
  */
 public class SingleTonMain2 {
     private SingleTonMain2() {
@@ -12,14 +13,17 @@ public class SingleTonMain2 {
 
     public static SingleTonMain2 getSingleTonMain2() {
         if (null == singleTonMain2) {
-            singleTonMain2 = new SingleTonMain2();
+            synchronized (SingleTonMain2.class) {
+                singleTonMain2 = new SingleTonMain2();
+            }
         }
         return singleTonMain2;
     }
+
     public static void main(String[] args) {
         SingleTonMain2 singleTonMain2 = getSingleTonMain2();
         SingleTonMain2 singleTonMain3 = getSingleTonMain2();
-        System.out.println(singleTonMain2==singleTonMain3);
+        System.out.println(singleTonMain2 == singleTonMain3);
 
     }
 }
