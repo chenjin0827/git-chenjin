576c8e05968c9a5091e9e41ae2a0f1071e859bd1
1219650953@qq.com <cj13032716642>
2019-12-10 18:22:00 +0800

    hfasdfasdf

--- chenjin-pattern/src/main/java/com/chenjin/pattern/agent/AgentMain1.java
+++
@@ -1,9 +0,0 @@
-package com.chenjin.pattern.agent;
-
-/**
- * 代理模式
- * 安全代理，看不到真实角色
- *静态代理需要自己去实现代理类
- */
-public class AgentMain1 {
-}
--- chenjin-pattern/src/main/java/com/chenjin/pattern/agent/House.java
+++
@@ -1,8 +0,0 @@
-package com.chenjin.pattern.agent;
-
-/**
- * 买房一个动作buy
- */
-public interface House {
-    void buy();
-}
