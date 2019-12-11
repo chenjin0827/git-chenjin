3d38a332b0c27a72f55d40a2150dfb1fddd0843f
1219650953@qq.com <cj13032716642>
2019-12-11 10:37:57 +0800

    20191211提交

--- chenjin-javastandard/src/main/java/com/chenjin/javastandard/cap1/Cap1Test.java
+++ chenjin-javastandard/src/main/java/com/chenjin/javastandard/cap1/Cap1Test.java
@@ -4,6 +4,13 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Random;
 
+/**
+ * 会变的常量 代码中应该保证常量就是常量，不能被改变
+ */
+interface Const {
+    int RAND_CONST = new Random().nextInt(10);
+}
+
 public class Cap1Test {
     @Test
     //本例中容易让人以为最后输出结果为22，但是结果是2
@@ -12,6 +19,12 @@ public class Cap1Test {
 //        long i = 1l;
         long i = 1L;
         System.out.println(i + i);
+    }
+
+    //测试常量会变 代码中不可取，避免这种情况 ，务必保证常量在运行期不变
+    @Test
+    public void test2() {
+        System.out.println(Const.RAND_CONST);
     }
 }
 
