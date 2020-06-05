34ea6c8e8f161bda74abe7ebe72e01991d45e9cf
1219650953@qq.com <cj13032716642>
2020-06-05 14:36:54 +0800

    20200605-14:35

--- chenjin-test/src/test/java/com/chenjin/test/TestJson.java
+++ chenjin-test/src/test/java/com/chenjin/test/TestJson.java
@@ -7,11 +7,7 @@ import java.util.Map;
 
 public class TestJson {
 public static void main(String []args){
-    Map<String, String> hashMap = new HashMap<>();
-    hashMap.put("aaa","aaa");
-    hashMap.put("bbb","bbb");
-    hashMap.put("ccc","ccc");
-    String s = JSON.toJSONString(hashMap);
-    System.out.println("s====="+s);
+    double ceil = Math.ceil(1 / 3);
+    System.out.println(ceil);
 }
 }
