e7c2bd6f761bc316a3f38156784fd5971b38524a
陈进 <1219650953@qq.com>
2020-09-10 14:53:56 +0800

    20200910 14:53

---
+++ chenjin-test/src/main/java/com/chenjin/test/TestIndexOf.java
@@ -0,0 +1,9 @@
+package com.chenjin.test;
+
+public class TestIndexOf {
+    public static void main(String[] args) {
+        String s="/msc-web-admin/sys/login.htmlx";
+        String s2="/msc-web-admin/s1ys2/login.htmlx";
+        System.out.println(s2.indexOf("/sys"));
+    }
+}
