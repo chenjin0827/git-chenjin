c6551076cc379bf46f25ee8af18c6e1860ff8fcc
陈进 <1219650953@qq.com>
2020-12-01 14:45:31 +0800

    11

--- chenjin-jvm/src/main/java/ex2/TestString.java
+++ chenjin-jvm/src/main/java/ex2/TestString.java
@@ -51,8 +51,8 @@ public class TestString {
         System.out.println("innerName:" + (name == innerName));
         System.out.println("innerCity:" + (city == innerCity));
         System.out.println("------------------------------");
-        System.out.println(name==student2.getName());
-        System.out.println(city==student2.getCity());
+        System.out.println(name == student2.getName());
+        System.out.println(city == student2.getCity());
         System.out.println("------------------------------");
         System.out.println(System.identityHashCode(student.getName()));
         System.out.println(System.identityHashCode(student.getCity()));
@@ -62,6 +62,16 @@ public class TestString {
         System.out.println(System.identityHashCode(city));
         System.out.println(System.identityHashCode(innerName));
         System.out.println(System.identityHashCode(innerCity));
+
+    }
+
+    public static void main(String[] args) {
+        String city="深圳";
+        Student student = new TestString().new Student();
+        student.setCity(args[0]);
+//        student.setCity("深圳");
+        System.out.println(student.getCity());
+        System.out.println(city==student.getCity());
 
     }
 }
